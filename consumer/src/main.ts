f2de6f3d4a15af8b8afc1c2d3995c903ea816687
Santiago Aguirre <santiago.aguirre@yara.com>
2023-11-27 15:35:10 +0100

    Implement consumer poc

--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Consumer } from 'sqs-consumer';
+import { Message, SQSClient } from '@aws-sdk/client-sqs';
+import { fromIni } from '@aws-sdk/credential-providers';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const sqsClient = new SQSClient({
+  endpoint: 'http://localhost:4566',
+  region: 'us-east-1',
+  credentials: fromIni({ profile: 'localstack' }),
+});
+
+const app = Consumer.create({
+  queueUrl:
+    'http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/field-deleted-queue',
+  pollingWaitTimeMs: 1000,
+  handleMessage(message: Message): Promise<Message | void> {
+    return new Promise(() => {
+      console.log(message.Body);
+    });
+  },
+  sqs: sqsClient,
+});
+
+app.start();
