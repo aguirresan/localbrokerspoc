739221e6fd9487ed550274cc13c87089ab942fca
Santiago Aguirre <santiago.aguirre@yara.com>
2023-12-05 09:14:47 +0100

    Route messages to two queues depending on content

--- tools/brokers/create-infra.sh
+++ tools/brokers/create-infra.sh
@@ -5,12 +5,22 @@ alias awslocal='aws --endpoint-url http://localhost:4566'
 eventBusName="yara-eu-events-bus"
 awslocal events create-event-bus --name $eventBusName
 
-queueName="field-events-queue"
+queueName="field-deleted-queue"
 queueUrl=$(awslocal sqs create-queue --queue-name $queueName --output text --query 'QueueUrl')
 queueArn=$(awslocal sqs get-queue-attributes --queue-url $queueUrl --attribute-names 'QueueArn' --output text --query 'Attributes.QueueArn')
 
-ruleName="field-updates-rule"
-awslocal events put-rule --event-bus-name $eventBusName --name $ruleName --event-pattern file:///etc/localstack/init/ready.d/eventpattern.json
-
+ruleName="field-deleted-rule"
+awslocal events put-rule --event-bus-name $eventBusName --name $ruleName --event-pattern file:///etc/localstack/init/ready.d/field-deleted-pattern.json
 awslocal events put-targets --event-bus-name $eventBusName --rule $ruleName --targets "Id"="1","Arn"="$queueArn"
+
+
+
+queueName="field-created-queue"
+queueUrl=$(awslocal sqs create-queue --queue-name $queueName --output text --query 'QueueUrl')
+queueArn=$(awslocal sqs get-queue-attributes --queue-url $queueUrl --attribute-names 'QueueArn' --output text --query 'Attributes.QueueArn')
+
+ruleName="field-created-rule"
+awslocal events put-rule --event-bus-name $eventBusName --name $ruleName --event-pattern file:///etc/localstack/init/ready.d/field-created-pattern.json
+awslocal events put-targets --event-bus-name $eventBusName --rule $ruleName --targets "Id"="1","Arn"="$queueArn"
+
 
