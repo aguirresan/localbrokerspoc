e12b5926996a7d8ad5e5e0469714acc280a94c9d
Santiago Aguirre <santiago.aguirre@yara.com>
2023-12-05 09:30:58 +0100

    Implement two consumers of differnt event types

---
+++ create-fields-consumer/src/main.ts
@@ -0,0 +1,29 @@
+import { Consumer } from 'sqs-consumer';
+import { DeleteMessageCommand, Message, SQSClient } from '@aws-sdk/client-sqs';
+import { fromIni } from '@aws-sdk/credential-providers';
+
+const sqsClient = new SQSClient({
+  endpoint: 'http://localhost:4566',
+  region: 'us-east-1',
+  credentials: fromIni({ profile: 'localstack' }),
+});
+
+const app = Consumer.create({
+  queueUrl:
+    'http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/field-created-queue',
+  pollingWaitTimeMs: 1000,
+  async handleMessage(message: Message): Promise<Message | void> {
+    await sqsClient.send(
+      new DeleteMessageCommand({
+        QueueUrl: this.queueUrl,
+        ReceiptHandle: message.ReceiptHandle,
+      }),
+    );
+    return new Promise(() => {
+      console.log(message.Body);
+    });
+  },
+  sqs: sqsClient,
+});
+
+app.start();
--- delete-fields-consumer/src/main.ts
+++ delete-fields-consumer/src/main.ts
@@ -1,5 +1,5 @@
 import { Consumer } from 'sqs-consumer';
-import { Message, SQSClient } from '@aws-sdk/client-sqs';
+import { DeleteMessageCommand, Message, SQSClient } from '@aws-sdk/client-sqs';
 import { fromIni } from '@aws-sdk/credential-providers';
 
 const sqsClient = new SQSClient({
@@ -10,9 +10,16 @@ const sqsClient = new SQSClient({
 
 const app = Consumer.create({
   queueUrl:
-    'http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/field-events-queue',
+    'http://sqs.us-east-1.localhost.localstack.cloud:4566/000000000000/field-deleted-queue',
   pollingWaitTimeMs: 1000,
-  handleMessage(message: Message): Promise<Message | void> {
+  async handleMessage(message: Message): Promise<Message | void> {
+    await sqsClient.send(
+      new DeleteMessageCommand({
+        QueueUrl: this.queueUrl,
+        ReceiptHandle: message.ReceiptHandle,
+      }),
+    );
+
     return new Promise(() => {
       console.log(message.Body);
     });
